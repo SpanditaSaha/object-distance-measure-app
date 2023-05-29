e0fed5394ee2d40c3363f0342602cf44cec38048
Spandita Saha <spandita406@gmail.com>
2023-05-29 16:19:18 +0530

    cmmit

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const router = express.router();
 const { promisify } = require('util');
 const { PythonShell } = require('python-shell');
 
@@ -10,11 +12,11 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/measure', async (req, res) => {
+router.get('/measure', async (req, res) => {
   const options = {
     mode: 'text',
     pythonOptions: ['-u'],
@@ -35,9 +37,12 @@ app.get('/measure', async (req, res) => {
   
 });
 
-const port = 8000;
-app.listen(port, () => console.log(`Server connected to ${port}`));
+// const port = 8000;
+// app.listen(port, () => console.log(`Server connected to ${port}`));
 
 process.on("unhandledRejection", (err) => {
   console.log(err.name, err.message);
 })
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
